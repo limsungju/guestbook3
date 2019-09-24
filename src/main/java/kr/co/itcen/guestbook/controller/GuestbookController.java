0640f8372749f436f3f45621d377be0a42df1687
limsungju <fullmoon4860@gmail.com>
2019-09-24 10:49:40 +0900

    guestbook3 - 삭제 추가

--- src/main/java/kr/co/itcen/guestbook/controller/GuestbookController.java
+++ src/main/java/kr/co/itcen/guestbook/controller/GuestbookController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -34,7 +35,7 @@ public class GuestbookController {
 	}
 	
 	@RequestMapping(value="/delete", method=RequestMethod.POST)
-	public String delete(GuestbookVo vo) {
+	public String delete(@ModelAttribute GuestbookVo vo) {
 		guestbookDao.delete(vo);
 		return "redirect:/";
 	}
